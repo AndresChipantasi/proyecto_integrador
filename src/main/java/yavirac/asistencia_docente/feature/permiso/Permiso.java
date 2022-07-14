f9e91305dd96d0ab7eeecdb9605b01b9e3534857
basganan <bas.ganan@yavirac.edu.ec>
2022-07-14 13:59:00 -0500

    backend

---
+++ src/main/java/yavirac/asistencia_docente/feature/permiso/Permiso.java
@@ -0,0 +1,27 @@
+package yavirac.asistencia_docente.feature.permiso;
+
+import java.sql.Timestamp;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Column;
+import org.springframework.data.relational.core.mapping.Table;
+
+import lombok.Data;
+
+@Data
+@Table("permisos")
+
+public class Permiso {
+
+
+    @Id
+    @Column("permiso_id")
+    private long permisoId;
+    private String name;
+    private Timestamp created;
+    private Timestamp updated;
+    private boolean enabled;
+
+    
+    
+}
---
+++ src/main/java/yavirac/asistencia_docente/feature/permiso/PermisoRepository.java
@@ -0,0 +1,8 @@
+package yavirac.asistencia_docente.feature.permiso;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface PermisoRepository extends CrudRepository<Permiso, Long> {
+    
+    
+}
