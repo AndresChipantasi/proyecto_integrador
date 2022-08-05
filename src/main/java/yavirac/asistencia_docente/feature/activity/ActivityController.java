145cabc059ee624d191b7bcd47f73345cc755deb
root <4ndy.chips5@gmail.com>
2022-08-05 19:02:14 +0000

    Backend funcionando

--- src/main/java/yavirac/asistencia_docente/feature/activity/ActivityController.java
+++ src/main/java/yavirac/asistencia_docente/feature/activity/ActivityController.java
@@ -1,6 +1,9 @@
 package yavirac.asistencia_docente.feature.activity;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,39 +12,43 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@CrossOrigin({"*"})
 @RequestMapping("/api/activity")
-public class ActivityController {
-    
+@CrossOrigin({"*"})
 
+public class ActivityController {
     @Autowired
+
     ActivityService activityService;
 
-    //CRUD
-
     @PostMapping("/save")
-    public Activity save(@RequestBody Activity activity){
+    public Activity save(@RequestBody Activity activity) {
         return activityService.save(activity);
     }
-    
     @GetMapping("/{id}")
     public Activity findById(@PathVariable long id){
-    return activityService.findById(id); 
+        return activityService.findById(id);
     }
-
-    @PutMapping("/update")
-    public Activity update(@RequestBody  Activity activity){
-
-    return activityService.save(activity);
-    }
-
-    @DeleteMapping("/delete/{id}")
-    public void deleteById(@PathVariable long id){
-        activityService.deleteById(id);
     
+    @PutMapping("/update")
+    public Activity update(@RequestBody Activity activity)
+    {
+        return activityService.save(activity);
     }
-
+    @DeleteMapping("/deleteById/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void delete(@PathVariable long id){
+        activityService.deleteById(id);
     }
+    @GetMapping("/findAll")
+    public List<Activity> findAll(){
+        return activityService.findAll();
+    }
+    @GetMapping("/findByName/{term}")
+    public List<Activity> findByName(@PathVariable String term){ //Mapea desde la URL con PathVariable
+        return activityService.findByName(term);
+    }
+}
--- src/main/java/yavirac/asistencia_docente/feature/activity/ActivityService.java
+++ src/main/java/yavirac/asistencia_docente/feature/activity/ActivityService.java
@@ -1,18 +1,19 @@
 package yavirac.asistencia_docente.feature.activity;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ActivityService {
+    
 
     @Autowired
     ActivityRepository activityRepository;
 
-    //CRUD = Create, Read, Update, Delete
-
     public Activity save(Activity activity){
-        return activityRepository.save(activity);
+       return activityRepository.save(activity);
     }
 
     public Activity findById(long id){
@@ -21,10 +22,18 @@ public class ActivityService {
 
     public Activity update(Activity activity){
         return activityRepository.save(activity);
+
     }
 
     public void deleteById(long id){
         activityRepository.deleteById(id);
     }
-    
+
+    public List<Activity> findAll(){
+        return activityRepository.findAll();
+    }
+
+    public List<Activity> findByName(String term){
+        return activityRepository.findByUserNameLikeIgnoreCase(term+"%");
+    }
 }
